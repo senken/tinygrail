f90bc140c12e07e41066e3853f35108d1174d11d
koorinyaa <1318844659@qq.com>
2026-04-19 16:10:30 +0800

    chore(deps): 升级TailwindCSS版本并引入DaisyUI

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,5 +3,25 @@ module.exports = {
     'postcss-nesting': {},
     tailwindcss: {},
     autoprefixer: {},
+    // 给所有样式添加.tinygrail前缀
+    'postcss-prefix-selector': {
+      prefix: '.tinygrail',
+      // 排除不需要添加前缀的选择器
+      exclude: [
+        /^html/,
+        /^body/,
+        /^:root/,
+        /\.tinygrail/,
+        /^@keyframes/,
+      ],
+      // 自定义转换函数
+      transform: function (prefix, selector, prefixedSelector) {
+        // 如果选择器已经包含.tinygrail，不添加前缀
+        if (selector.includes('.tinygrail')) {
+          return selector;
+        }
+        return prefixedSelector;
+      },
+    },
   },
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/container-queries")],
+  plugins: [require("@tailwindcss/container-queries"), require("daisyui")],
 };
